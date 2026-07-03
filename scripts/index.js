d1679fe06835e35e9332f0cb68c74bb131336264
Panthera <101964317+PantheraDigital@users.noreply.github.com>
2026-07-03 16:38:58 -0700

    Remove commented-out GitHub fetch code
    Removed commented-out code for fetching GitHub data.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 export default {
 
   async fetch(request, env) {
-    /*
     try {
         const response = await env.GET_GITHUB_JSON.fetch();
         if (!response.ok) { throw new Error(`Worker responded with status: ${response.status}`); }
@@ -11,7 +10,6 @@ export default {
     } catch (error) {
         console.error("Failed to load GitHub data:", error.message);
     }
-    */
     return env.ASSETS.fetch(request);
   }
   
