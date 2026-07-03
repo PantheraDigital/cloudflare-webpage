e931bf18d35796d3b753df62b7d033951b992a61
Panthera <101964317+PantheraDigital@users.noreply.github.com>
2026-07-03 16:23:49 -0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ import { WorkerEntrypoint } from "cloudflare:workers";
 export default class extends WorkerEntrypoint {
 
   async fetch(request, env) {
-    console.log(request);
+    /*
     try {
         const response = await env.GET_GITHUB_JSON.fetch();
         if (!response.ok) { throw new Error(`Worker responded with status: ${response.status}`); }
@@ -13,7 +13,7 @@ export default class extends WorkerEntrypoint {
     } catch (error) {
         console.error("Failed to load GitHub data:", error.message);
     }
-    
+    */
     return this.env.ASSETS.fetch(request);
   }
   
