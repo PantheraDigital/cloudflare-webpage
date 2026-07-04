3698102b6cc11460ff7230db8791b4531004a71d
Panthera <101964317+PantheraDigital@users.noreply.github.com>
2026-07-04 15:24:41 -0700

    Refactor GitHub data fetching and placeholder replacement
    Updated GitHub data retrieval and handling logic.

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,25 @@ export default {
         const url = new URL(request.url);
         
         if (url.pathname === "/scripts/frontend.js") {
+            const githubValue = null;
             try {
                 const response = await env.GET_GITHUB_JSON.fetch("https://dummy/");
-                if (!response.ok) throw new Error(`GitHub API failed. Status: ${response.status}`);
-                
-                const githubValue = await response.json();
+                if (!response.ok) throw new Error(`GitHub data retrieval failed. Status: ${response.status}`);
+                githubValue = await response.json();
+            } catch (error) {
+                console.error("Failed to load GitHub data:", error.message);
+            }
+
+            try {
                 const assetResponse = await env.ASSETS.fetch(request);
                 let jsText = await assetResponse.text();
                 
-                jsText = jsText.replace("INITIAL_DATA_PLACEHOLDER", JSON.stringify(githubValue));
-                
+                jsText = jsText.replace("INITIAL_DATA_PLACEHOLDER", (githubValue) ? JSON.stringify(githubValue) : "{}");
                 return new Response(jsText, {
                     headers: { "content-type": "application/javascript;charset=UTF-8" }
                 });
             } catch (error) {
                 console.error("Data injection failed, serving original file:", error.message);
-                return env.ASSETS.fetch(request);
             }
         }
 
