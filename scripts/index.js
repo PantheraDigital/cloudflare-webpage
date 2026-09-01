e00299d39df7da1931ecd700d3c41b24d20e70f4
Panthera <101964317+PantheraDigital@users.noreply.github.com>
2026-08-31 23:36:48 -0700

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -34,7 +34,7 @@ class KVContentHandler {
 
     async element(el) {
         try {
-            console.log(`Rendering ${this.type} | Keys:`, JSON.stringify(this.keys.map(k => k.name)));
+            //console.log(`Rendering ${this.type} | Keys:`, JSON.stringify(this.keys.map(k => k.name)));
             // KV get() - max keys 100, response size limit 25MB
             const batches = createBatches(this.keys);
             let entryCount = 0;
@@ -214,14 +214,11 @@ async function renderPage(env) {
         throw new Error(`Failed to load base HTML: ${htmlRes.status} ${htmlRes.statusText}`);
     }
 
-    console.log("postKVList: ", JSON.stringify(postKVList));
-    console.log("projectKVList: ", JSON.stringify(projectKVList));
-
     const postKeys = postKVList.keys.filter((entry) => entry.metadata?.live === true);
     const projectKeys = projectKVList.keys.filter((entry) => entry.metadata?.live === true);
 
-    console.log("postKeys: ", JSON.stringify(postKeys));
-    console.log("projectKeys: ", JSON.stringify(projectKeys));
+    if (postKeys.length === 0) { console.warn("Post Keys empty"); }
+    if (projectKeys.length === 0) { console.warn("Project Keys empty"); }
 
     const rewriter = new HTMLRewriter()
         .on('div#posts-container', new KVContentHandler(env, postKeys, "post", postTemplate, env.POST_PREFIX))
