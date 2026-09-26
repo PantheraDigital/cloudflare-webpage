0f22d09d7d46f6f447b46f86690cbc5ba831a1eb
Panthera <101964317+PantheraDigital@users.noreply.github.com>
2026-09-25 21:55:46 -0700

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -231,7 +231,7 @@ async function renderPage(env) {
         throw new Error(`Failed to load base HTML: ${htmlRes.status} ${htmlRes.statusText}`);
     }
 
-    const postKeys = postKVList.keys.filter((entry) => {console.log(entry.metadata?.live); return entry.metadata?.live === true;});
+    const postKeys = postKVList.keys.filter((entry) => {console.log(entry.metadata.live); return entry.metadata?.live === true;});
     const projectKeys = projectKVList.keys.filter((entry) => entry.metadata?.live === true);
 
     if (postKeys.length === 0) { console.warn("Article Keys empty"); }
